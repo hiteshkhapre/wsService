f57e5e9c76b940042995c4044756c811aa1f587b
hiteshkhapre <hiteshkhapre@ubuntuhk>
2014-12-23 00:24:44 +0000

    22Dec2014

---
+++ src/java/customer/CustomerProfile.java
@@ -0,0 +1,87 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package customer;
+
+/**
+ *
+ * @author hiteshkhapre
+ */
+public class CustomerProfile {
+    
+     private String cust_ID;
+            private String cust_firstname;
+            private String cust_lastname;
+            private String cust_Addressline1;
+            private String cust_Addressline2;
+            private String cust_City;
+            private String cust_Contactnumber;
+            private String cust_Email; 
+
+    public String getCust_ID() {
+        return cust_ID;
+    }
+
+    public void setCust_ID(String cust_ID) {
+        this.cust_ID = cust_ID;
+    }
+
+    public String getCust_firstname() {
+        return cust_firstname;
+    }
+
+    public void setCust_firstname(String cust_firstname) {
+        this.cust_firstname = cust_firstname;
+    }
+
+    public String getCust_lastname() {
+        return cust_lastname;
+    }
+
+    public void setCust_lastname(String cust_lastname) {
+        this.cust_lastname = cust_lastname;
+    }
+
+    public String getCust_Addressline1() {
+        return cust_Addressline1;
+    }
+
+    public void setCust_Addressline1(String cust_Addressline1) {
+        this.cust_Addressline1 = cust_Addressline1;
+    }
+
+    public String getCust_Addressline2() {
+        return cust_Addressline2;
+    }
+
+    public void setCust_Addressline2(String cust_Addressline2) {
+        this.cust_Addressline2 = cust_Addressline2;
+    }
+
+    public String getCust_City() {
+        return cust_City;
+    }
+
+    public void setCust_City(String cust_City) {
+        this.cust_City = cust_City;
+    }
+
+    public String getCust_Contactnumber() {
+        return cust_Contactnumber;
+    }
+
+    public void setCust_Contactnumber(String cust_Contactnumber) {
+        this.cust_Contactnumber = cust_Contactnumber;
+    }
+
+    public String getCust_Email() {
+        return cust_Email;
+    }
+
+    public void setCust_Email(String cust_Email) {
+        this.cust_Email = cust_Email;
+    }
+    
+}
--- src/java/ws/CustomerWebService.java
+++ src/java/ws/CustomerWebService.java
@@ -5,9 +5,16 @@
  */
 package ws;
 
+import customer.CustomerProfile;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.annotation.Resource;
 import javax.jws.WebService;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
+import javax.sql.DataSource;
 
 /**
  *
@@ -15,17 +22,40 @@ import javax.jws.WebParam;
  */
 @WebService(serviceName = "CustomerWebService")
 public class CustomerWebService {
+ @Resource(name = "mySQLABC")
+    private DataSource mySQLABC;
 
     /**
      * Web service operation
      */
-    @WebMethod(operationName = "getNameDetails")
-    public String getNameDetails(@WebParam(name = "customerID") String customerID) {
+    @WebMethod(operationName = "getMyProfile")
+    public CustomerProfile getMyProfile(@WebParam(name = "customer_username") String customer_username) {
         //TODO write your implementation code here:
-        
-        
-        
-        
-        return null;
+        CustomerProfile cust_profile = null;
+        try {
+            //TODO write your implementation code here:
+            Connection conn = mySQLABC.getConnection();
+            PreparedStatement pstat = conn.prepareStatement("SELECT * FROM `ABC Bank`.Customer where Cust_Username = ?");
+            pstat.setString(1,customer_username);
+            ResultSet rs = pstat.executeQuery();
+            cust_profile = new CustomerProfile();
+            
+            while(rs.next())
+            {
+               cust_profile.setCust_ID(rs.getString("cust_ID"));
+              cust_profile.setCust_firstname(rs.getString("cust_Firstname"));
+             cust_profile.setCust_lastname(rs.getString("cust_Lastname"));
+             cust_profile.setCust_Addressline1(rs.getString("cust_Addressline1"));
+             cust_profile.setCust_Addressline2(rs.getString("cust_Addressline2"));
+             cust_profile.setCust_City(rs.getString("cust_City"));
+             cust_profile.setCust_Contactnumber(rs.getString("cust_Contactnumber"));
+             cust_profile.setCust_Email(rs.getString("cust_Email"));
+            }
+            
+            
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return cust_profile;
     }
 }
